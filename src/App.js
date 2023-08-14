1507398a50574cec80892ff9c06bda0de193bc43
BLewis <45450044+BLewis-Keyin@users.noreply.github.com>
2023-08-14 15:04:34 +0000

    Fixed it so it actually works 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -6,43 +6,22 @@ import Home from './component/home';
 import About from './component/about';
 import Services from './component/services';
 import Contact from './component/contact';
-import { TaskProvider } from './TaskContext';
 import { ShoppingCartProvider } from './component/ShoppingCartContext'
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
 
-  const addTask = () => {
-    if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
-      setNewTask('');
-    }
-  };
 
-  const removeTask = (index) => {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
-    setTasks(updatedTasks);
-  };
+
 
   return (
-    <TaskProvider>
       <div className="app">
         <BrowserRouter>
           <nav>
             <ul>
-              <li>
                 <Link to="/">Home</Link>
-              </li>
-              <li>
                 <Link to="/about">About</Link>
-              </li>
-              <li>
                 <Link to="/services">Services</Link>
-              </li>
-              <li>
                 <Link to="/contact">Contact</Link>
-              </li>
             </ul>
           </nav>
 
@@ -52,32 +31,8 @@ const App = () => {
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
-
-          <h1 className="app-title">To-Do List</h1>
-          <div className="task-input">
-            <input
-              type="text"
-              placeholder="Enter a new task"
-              value={newTask}
-              onChange={(e) => setNewTask(e.target.value)}
-            />
-            <button className="add-button" onClick={addTask}>
-              Add Task
-            </button>
-          </div>
-          <ul className="task-list">
-            {tasks.map((task, index) => (
-              <li key={index} className="task-item">
-                <span>{task}</span>
-                <button className="delete-button" onClick={() => removeTask(index)}>
-                  Delete
-                </button>
-              </li>
-            ))}
-          </ul>
         </BrowserRouter>
       </div>
-    </TaskProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ShoppingCartProvider } from './component/ShoppingCartContext'; // Import the context
-
 import App from './App'; // Your main app component
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
-    <Router>
-      <ShoppingCartProvider>
-        <App />
-      </ShoppingCartProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+    </BrowserRouter>  
+    <App />
+    <ShoppingCartProvider>
+    </ShoppingCartProvider>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </React.StrictMode>
+);
