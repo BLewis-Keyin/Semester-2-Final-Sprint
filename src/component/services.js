4e1eeb8db7ebb312172f8a2ca1c679de99175de5
BLewis <45450044+BLewis-Keyin@users.noreply.github.com>
2023-08-18 10:48:55 +0000

    Services page completed . Now in a passable state. Need to do some finishing stuff before hand in to fufill requirement for full marks

--- src/component/services.js
+++ src/component/services.js
@@ -4,7 +4,26 @@ import CheckMarks from '../images/fi-rr-list-check.png'
 import FollowIcon from '../images/fi-rr-following.png'
 
 const services = () => {
-  return <div className='other-background'><div className='services-container'>Our Services<div className='services-info-container'><div className='services-image-container'><img src={CheckMarks} className='check-marks-image'></img> </div><div className='services-image-container'><img src={FollowIcon} className='follow-image'></img></div></div></div></div>;
+  return (
+    <div className='other-background'>
+      <div className='services-container'>
+        <h2>Our Services</h2>
+        <div className='services-content'>
+          <div className='services-image-container'>
+            <img src={CheckMarks} className='check-marks-image' alt='Check Marks' />
+          </div>
+          <div className='services-image-container'>
+            <img src={FollowIcon} className='follow-image' alt='Follow Icon' />
+          </div>
+        </div>
+        <div className='services-text-box'>
+          <div className='services-text'>Personal Task<br /> Management</div>
+          <div className='services-text'>Team<br /> Collaboration</div>
+        </div>
+      </div>
+    </div>
+  );
+
 };
 
 export default services;
